3cfe0939afbf4781d777a55eaf4976068208a190
Wansmile55140 <42538817+Wansmile55140@users.noreply.github.com>
2019-09-08 15:12:49 +0700

    Add files via upload

---
+++ wan.java
@@ -0,0 +1,12 @@
+public class wan {
+  public static void main(String wan[]) {
+    int wan1=Integer.parseInt(wan[0]);
+    int wan2=Integer.parseInt(wan[1]);
+    int wan3=Integer.parseInt(wan[2]);
+	System.out.println("plus="+(wan1+wan2+wan3));
+        System.out.println("minus="+((wan1-wan2)-wan3));
+System.out.println("multi="+((wan1*wan2)*wan3)); 
+System.out.println("div="+((wan1/wan2)/wan3));
+  
+  }
+}
